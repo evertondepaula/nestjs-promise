acaea1de486cec0ebc05011d26c055ee7b123e44
Everton de Paula <everton_to@live.com>
2020-08-01 12:05:26 -0300

    Tests async and sync resources

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import User from './user.model';
-import { resolve } from 'path';
-
 
 @Controller()
 export class AppController {
   
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  async getUsers(): Promise<any> {
+  @Get('/async/users')
+  async getAsyncUsers(): Promise<User[]> {
 
     try {
-      let promises = [];
+      let promises:Promise<User>[] = [];
 
       for (let i = 1; i <= 10; i++) {
-        promises.push(new Promise((resolve, reject) => {
-          resolve(this.appService.getUser(Math.floor(Math.random() * 11) +1));
+        promises.push(new Promise((resolve) => {
+          let user:Promise<User> = this.appService.getUser(Math.floor(Math.random() * 11) +1);
+          resolve(user);
         }));
       }
 
@@ -25,5 +24,18 @@ export class AppController {
     } catch (error) {
       console.error('OTRO GATO: ', error.message);
     }
+  }
+
+  @Get('/sync/users')
+  async getSyncUsers(): Promise<User[]> {
+
+    let users:User[] = [];
+    
+    for (let i = 1; i <= 10; i++) {
+      const user:User = await this.appService.getUser(Math.floor(Math.random() * 11) +1);
+      users.push(user);
+    }
+
+    return users;
   }
 }
